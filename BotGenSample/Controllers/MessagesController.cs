5afc7edd17469ed61522af86eb48459248f46ce3
ericyeh92094 <ericyeh92094@hotmail.com>
2016-09-07 11:10:21 +0800

    Add sample logics

--- BotGenSample/Controllers/MessagesController.cs
+++ BotGenSample/Controllers/MessagesController.cs
@@ -26,7 +26,8 @@ namespace BotGenSample
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                Activity reply = await HandleBotMessage(activity);
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
@@ -64,6 +65,95 @@ namespace BotGenSample
             }
 
             return null;
+        }
+
+        private async Task<Activity> HandleBotMessage(Activity message)
+        {
+            LuisHelp cognitive = new LuisHelp();
+            string CarCaringString;
+            CarCaringLUIS carLUIS = await LuisHelp.GetEntityFromLUIS(message.Text);
+            if (carLUIS.intents.Count() > 0)
+            {
+                Intent maxscore_intent = carLUIS.intents.Max();
+
+                switch (maxscore_intent.intent)
+                {
+                    case "StoreLocation":
+                        string storeURL = "";
+                        CarCaringString = cognitive.GetStoreLocation(carLUIS, ref storeURL);
+                        break;
+                    case "CheckItem":
+                        CarCaringString = cognitive.GetItem(carLUIS);
+                        break;
+                    case "News":
+                        CarCaringString = cognitive.GetNews(carLUIS);
+                        break;
+                    case "Tips":
+                        CarCaringString = await cognitive.GetTips(carLUIS, "FromUser", "ToUser");
+                        break;
+                    default:
+                        CarCaringString = await cognitive.GetBingSearch(message.Text); // "您可以到网站去查询我门的最新讯息。";
+
+
+                        break;
+                }
+            }
+            else
+            {
+                CarCaringString = "您可以到网站去查询我门的最新讯息。";
+            }
+            // return our reply to the user
+            return message.CreateReply(CarCaringString);
+        }
+    }
+    public class ReplyType
+    {
+        /// <summary>
+        /// 普通文本消息
+        /// </summary>
+        public static string Message_Text
+        {
+            get { return @"<xml>
+                        <ToUserName><![CDATA[{0}]]></ToUserName>
+                        <FromUserName><![CDATA[{1}]]></FromUserName>
+                        <CreateTime>{2}</CreateTime>
+                        <MsgType><![CDATA[text]]></MsgType>
+                        <Content><![CDATA[{3}]]></Content>
+                        </xml>"; }
+        }
+        /// <summary>
+        /// 图文消息主体
+        /// </summary>
+        public static string Message_News_Main
+        {
+            get
+            {
+                return @"<xml>
+                        <ToUserName><![CDATA[{0}]]></ToUserName>
+                        <FromUserName><![CDATA[{1}]]></FromUserName>
+                        <CreateTime>{2}</CreateTime>
+                        <MsgType><![CDATA[news]]></MsgType>
+                        <ArticleCount>{3}</ArticleCount>
+                        <Articles>
+                        {4}
+                        </Articles>
+                        </xml> ";
+            }
+        }
+        /// <summary>
+        /// 图文消息项
+        /// </summary>
+        public static string Message_News_Item
+        {
+            get
+            {
+                return @"<item>
+                        <Title><![CDATA[{0}]]></Title> 
+                        <Description><![CDATA[{1}]]></Description>
+                        <PicUrl><![CDATA[{2}]]></PicUrl>
+                        <Url><![CDATA[{3}]]></Url>
+                        </item>";
+            }
         }
     }
 }
